fa42e69dde253494b5092ca38ad64afd658df93a
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-09 23:01:30 +0900

    Create 18108.java

---
+++ ch01/18108.java
@@ -0,0 +1,13 @@
+// 단기 계산법 : 단기는 기원전 233년 (고조선 건국한해) 당해년도+건국한해 2333  
+// 불기 계산법 : 당해년도 + 부처님열반드신해 544
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    int year = s.nextInt();
+	    
+		System.out.println(year - 543 );
+	}
+}
