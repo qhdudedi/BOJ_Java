d7a8f02b294a1bdbcbe0c06efc8b7d65b81d9a68
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-10 15:37:52 +0900

    Create 10171.java

---
+++ ch01/10171.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    System.out.println("\\    /\\");
+	    System.out.println(" )  ( ')");
+	    System.out.println("(  /  )");
+	    System.out.println(" \\(__)|");
+	}
+}
+
+//역슬래쉬 출력 문제 -> 역슬래쉬를 출력할 때는 \\ 2개를 적어줘야 \ 1개가 출력된다.
