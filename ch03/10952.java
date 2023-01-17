f98279f5fd611090f89113838ef7954bda5cc99a
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-17 16:46:52 +0900

    Update 10952.java

--- ch03/10952.java
+++ ch03/10952.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		while(true){
+		    int a = s.nextInt();
+		    int b = s.nextInt();
+		    
+		    if(a + b == 0) {
+		        break;
+		    }
+		    System.out.println(a+b);
+		}
+		s.close();
+	}
+}
