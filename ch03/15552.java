c1498af81af13d6ada0e6a82d3eef6f86d720cfa
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-17 15:50:16 +0900

    Update 15552.java

--- ch03/15552.java
+++ ch03/15552.java
@@ -1,9 +1,8 @@
 import java.io.*;
 import java.util.StringTokenizer;
 class Main {
-	public static void main(String[] args){ // 예외처리 필수! 또는 throwsIOException해주기
+	public static void main(String[] args){ 
 		try{
-			// 콘솔에서 입력 받을 경우
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			
@@ -30,3 +29,26 @@ class Main {
 	
 	}
 }
+
+// class Main {
+// 	public static void main(String[] args) throws IOException { 
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+// 		int num = Integer.parseInt(br.readLine());
+// 		StringTokenizer st;
+// 		int a, b;
+// 		for (int i=0; i< num; i++){
+// 			st = new StringTokenizer(br.readLine());
+// 			a = Integer.parseInt(st.nextToken());
+// 			b = Integer.parseInt(st.nextToken());
+			    
+// 			bw.write((a+b) + "\n");
+// 		}
+// 		br.close();
+			
+// 		bw.flush();
+// 		bw.close();
+// 	}
+// }
+
