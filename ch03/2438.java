b97d093efd2b879a6b95489093d2b61aad2a23d0
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-17 16:21:17 +0900

    Update 2438.java

--- ch03/2438.java
+++ ch03/2438.java
@@ -1,1 +1,40 @@
+// BufferedReader 버퍼 사
+import java.io.*;
+import java.util.StringTokenizer;
+class Main {
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+		int num = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		// int a, b;
+		for (int i=0; i< num; i++){
+		    for(int j=0; j< i+1; j++){
+		        bw.write("*");    
+		    }
+		    bw.write("\n");
+		   
+		}
+		br.close();
+			
+		bw.flush();
+		bw.close();
+	}
+}
 
+// Scanner 사용
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		for (int i=0; i <n; i++){
+		    for(int j=0; j<i+1; j++){
+		        System.out.print("*");
+		    }
+		    System.out.println("");
+		}
+	}
+}
