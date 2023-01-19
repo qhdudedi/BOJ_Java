f04c47145eabd0442c9e28591aa53c4cbe7583c4
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-19 23:01:23 +0900

    Update 10951.java

--- ch03/10951.java
+++ ch03/10951.java
@@ -1,2 +1,13 @@
-
-# 21
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    
+	    while(s.hasNextInt()){
+	        int a = s.nextInt();
+	        int b = s.nextInt();
+            System.out.println(a+b);
+	    }
+	}
+}
