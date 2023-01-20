2666d053be3a9bbeb3fbe948b3869b876d7da113
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-20 20:40:18 +0900

    Update 1110.java

--- ch03/1110.java
+++ ch03/1110.java
@@ -1,1 +1,15 @@
-#12321
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int num = sc.nextInt();
+	    int count = 0;
+	    int copy_num = num;
+	    do{
+	        num = ((num % 10)*10) +( ((num / 10)+(num % 10)) % 10);
+	        count++;
+	    }while( copy_num != num);
+		System.out.println(count);    
+	}
+}
