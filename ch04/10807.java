d06f2b591a93d0b1851efccbb6160f3528156281
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-20 23:14:51 +0900

    Update 10807.java

--- ch04/10807.java
+++ ch04/10807.java
@@ -1,1 +1,31 @@
+/* package BOJ;
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class b_10807 {
+
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    int N = Integer.parseInt(br.readLine());
+	    int[] arr = new int[N];
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    
+	    
+	    for(int i=0; i<= arr.length; i++){
+	        arr[i]= Integer.parseInt(st.nextToken());
+	    }
+	    
+	    int find = Integer.parseInt(br.readLine());
+	    int num_cnt=0;
+	    for(int i=0; i<= arr.length; i++){
+	        if(arr[i] == find){
+	            num_cnt++;
+	        }
+	    }
+	    System.out.println(num_cnt);
+	   
+		br.close();
+	}
+}
+*/
