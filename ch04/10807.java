37a7ff8f408774d6abc045950401b0a9e0bf0446
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-20 23:31:52 +0900

    Update 10807.java

--- ch04/10807.java
+++ ch04/10807.java
@@ -1,3 +1,31 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int N = sc.nextInt();
+	    int[] arr = new int[N];
+	    
+	    
+	    for(int i=0; i< arr.length; i++){
+	        int num = sc.nextInt();
+	        arr[i]= num;
+	    }
+	    
+	    int find = sc.nextInt();
+	    int num_cnt=0;
+	    for(int i=0; i< arr.length; i++){
+	        if(arr[i] == find){
+	            num_cnt++;
+	        }
+	    }
+	    System.out.println(num_cnt);
+	}
+}
+
 /* package BOJ;
 import java.io.*;
 import java.util.StringTokenizer;
