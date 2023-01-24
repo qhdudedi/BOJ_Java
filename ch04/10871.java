1ec8a3a76f773bcffdb00f8a91d184195ebb0f6a
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-24 20:46:38 +0900

    Update 10871.java

--- ch04/10871.java
+++ ch04/10871.java
@@ -1,1 +1,16 @@
-##
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int a = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            int num = s.nextInt();
+            if(num < a ){
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
+    
