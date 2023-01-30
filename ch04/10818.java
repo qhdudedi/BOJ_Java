38f1379a8b5414e1c47eb24a8521819d69acfd18
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-31 00:39:20 +0900

    Update 10818.java

--- ch04/10818.java
+++ ch04/10818.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		int[] arr = new int[num];
+		
+		for(int i=0; i< num; i++){
+		    arr[i] = s.nextInt();
+		}
+		int max = arr[0];
+		for(int i=0; i< num; i++){
+		    if(arr[i] > max){
+		        max = arr[i];
+		    }
+	    }
+	    int min = arr[0];
+	    for(int i=0; i<num; i++){
+	        if(arr[i] < min){
+	            min = arr[i];
+	        }
+	    }
+	    System.out.print(min+" "+max);
+    }
+}
