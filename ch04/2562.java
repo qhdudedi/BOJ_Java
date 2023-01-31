c53185389befc61b0006f0f8209fc75e12e6df1a
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-31 23:56:53 +0900

    Update 2562.java

--- ch04/2562.java
+++ ch04/2562.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
-
-class Main{ 
-    public static void main(String[] args){
-      Scanner s = new Scanner(System.in);
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    int[] arr = new int[10];
+		int max =arr[1];
+		int num =0;
+		for(int i=1; i<arr.length; i++){
+		    arr[i] = s.nextInt();
+		    if(arr[i] > max){
+		        max = arr[i];
+		        num = i;
+		    }
+		}
+		System.out.println(max);
+		System.out.println(num);
+	}
 }
