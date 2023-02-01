715764902fec652ccc93acb62af7836dbc98c454
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-02-01 22:43:56 +0900

    Update 5597.java

--- ch04/5597.java
+++ ch04/5597.java
@@ -15,24 +15,3 @@ public class Main
 	        br.close();
 	}
 }
-
-// import java.util.*;
-
-// public class b_5597 {
-// 	public static void main(String[] args) {
-// 		Scanner s = new Scanner(System.in);
-// 		boolean[] arr = new boolean[31];
-		
-// 		for(int i=1; i< 29; i++){
-//             int stu = s.nextInt();
-//             arr[stu] = true;
-// 		}
-// 		for (int i = 1; i< arr.length; i++) {
-// 			if (arr[i] != true)
-// 				System.out.println(i);
-// 		}
-// 		s.close();
-		
-// 	}
-
-// }
