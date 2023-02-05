b7768f9d6ebcacf5b3a7246f91f74d336391f93c
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-02-05 23:10:38 +0900

    Update 3052.java

--- ch04/3052.java
+++ ch04/3052.java
@@ -1,8 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class main {
-  public static void main(String[] args){
-      Scanner s = new Scanner(System.in);
-  }
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] arr = new int[10];
+		boolean bl;
+		int cnt = 0; 
+
+		for(int i=0; i<arr.length; i++ ) {
+			arr[i] = Integer.parseInt(br.readLine()) % 42;
+		}
+		for(int i=0; i<10; i++ ) {
+			bl = false; 
+			for(int k=i+1; k<arr.length; k++ ) {
+				if( arr[i] == arr[k] ) { 
+					bl = true; 
+					break; 
+				}
+			}
+			if( bl == false ) {
+				cnt++; 
+			}
+		}
+		System.out.println(cnt);
+	}
 }
