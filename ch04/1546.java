71a957baefefde4ed0aae33e371d9b48dd8cda82
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-02-06 16:33:58 +0900

    Update 1546.java

--- ch04/1546.java
+++ ch04/1546.java
@@ -1,1 +1,28 @@
 
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    int n = s.nextInt();
+	    double[] arr = new double[n];
+	    
+	    double max = arr[0];
+	    double sum=0;
+	    for(int i=0; i<arr.length; i++){
+	        arr[i] = s.nextDouble();
+	        if(arr[i] > max){
+	            max = arr[i];
+	        } 
+	        System.out.println(max);
+	        
+	        for(i=0; i<arr.length; i++){
+	            arr[i] = arr[i]/max*100;
+	            sum += arr[i];
+	        }
+	    }
+	    double avg = sum /n;
+	    System.out.println(avg);
+	}
+}
