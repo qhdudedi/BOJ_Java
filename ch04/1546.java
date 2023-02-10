cf53307e9501b93d892937b92177f3f5e6382267
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-02-10 20:57:02 +0900

    Update 1546.java

--- ch04/1546.java
+++ ch04/1546.java
@@ -24,3 +24,31 @@ public class b_1546 {
 	}
 
 }
+
+package ch04;
+import java.io.*;
+import java.util.StringTokenizer;
+public class b_1546 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int n = Integer.parseInt(br.readLine());
+	    double[] arr = new double[n];
+	    StringTokenizer st = new StringTokenizer(br.readLine()," ");
+	    
+	    double max = arr[0];
+	    double sum=0;
+	    for(int i=0; i<arr.length; i++){
+	        arr[i] = Double.parseDouble(st.nextToken());
+	        if(arr[i] > max){
+	            max = arr[i];
+	        }
+	    }
+	    for(int i=0; i<arr.length; i++){
+            arr[i] = arr[i]/max*100;
+            sum += arr[i];
+        }
+	    double avg = sum /n;
+	    System.out.println(avg);
+	}
+
+}
