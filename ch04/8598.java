011f4482e71b2567cec063e72c6e75a28e01ee68
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-02-10 22:11:39 +0900

    Update 8598.java

--- ch04/8598.java
+++ ch04/8598.java
@@ -1,4 +1,34 @@
+package ch04;
 import java.io.*;
 
-public class{
+public class b_8598 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		String[] arr = new String[num];
+		
+		for(int i=0; i<num; i++) {
+			arr[i] = br.readLine();
+		}
+		for(int i=0; i<num; i++) {
+		int cnt = 0;
+		int sum = 0;
+			for(int j=0; j < arr[i].length(); j++) {
+				if(arr[i].charAt(j) == 'O') {
+					cnt++;
+				}
+				else {
+					cnt = 0;
+				}
+				sum += cnt;
+			}
+			sb.append(sum).append('\n');
+		}
+		System.out.print(sb);
+	}
+
 }
