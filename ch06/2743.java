8fc30f4380db4b72070085cc8d8c2b17db3ef4dd
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-04-09 23:13:50 +0900

    create 2743.java

--- ch06/2743.java
+++ ch06/2743.java
@@ -1,1 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class boj_2743 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.print(s.length());
+    }
+}
