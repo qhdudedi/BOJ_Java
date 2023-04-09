903f57845ed03fb8bdade764280eda1f71db2126
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-04-09 23:43:08 +0900

    create 9086.java

--- ch05/9086.java
+++ ch05/9086.java
@@ -1,1 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            String s = br.readLine();
+            sb.append(s.charAt(0));
+            sb.append(s.charAt(s.length()-1)+"\n");
+        }
+        System.out.println(sb);
+
+    }
+}
