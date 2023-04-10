362e66e4c954d28c9710b4d2cbf8c30f7a89e444
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-04-10 16:03:28 +0900

    create 11654.java

--- ch05/11654.java
+++ ch05/11654.java
@@ -1,1 +1,10 @@
-import
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int cd = System.in.read();
+        System.out.println(cd);
+    }
+}
+// System.in 은 byte 값으로 문자 한 개만 읽으면서 해당 문자에 대응되는 아스키 코드 값 저장 가능
+// ** 예외처리를 필수로 해줘야 함
