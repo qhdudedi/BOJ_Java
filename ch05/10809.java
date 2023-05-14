6e2a349ccd6fd27f5a94fe43452d68fb35fd3de5
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-05-14 14:22:19 +0900

    commit

--- ch05/10809.java
+++ ch05/10809.java
@@ -1,1 +1,25 @@
+package DoIt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_10809 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int arr[] = new int[26];
+        for(int i=0; i<26; i++){
+            arr[i] = -1;
+        }
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(arr[ch-'a'] == -1){
+                arr[ch-'a'] = i;
+            }
+        }
+        for(int var : arr){
+            System.out.print(var+" ");
+        }
+    }
+}
