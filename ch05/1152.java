386f0c831e2d479b399d58eb3d63470170e34528
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-05-21 23:07:45 +0900

    Update 1152.java

--- ch05/1152.java
+++ ch05/1152.java
@@ -1,1 +1,11 @@
 
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		System.out.print(st.countTokens());
+	}
+}
